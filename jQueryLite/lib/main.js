ece7553789450d04d652dfec83bb7eef3357637a
Matthew Gershen <matthewgershen@gmail.com>
2018-09-25 11:41:16 -0400

    Add document ready and extend functionality

--- jQueryLite/lib/main.js
+++ jQueryLite/lib/main.js
@@ -3,8 +3,19 @@ const DOMNodeCollection = require('./dom_node_collection.js');
 
 function $l(selector) {
   let nodeArr;
+  let callbacks = [];
 
-  if ( selector instanceof HTMLElement ) {
+  if (selector instanceof Function) {
+    callbacks.push(selector);
+    let stateCheck = setInterval(() => {
+      if (document.readyState === 'complete') {
+        clearInterval(stateCheck);
+        callbacks.forEach((fn)=>{
+          fn();
+        });
+    }
+  }, 100);
+  } else if (selector instanceof HTMLElement ) {
     nodeArr = [selector];
   } else {
     let nodelist = document.querySelectorAll(selector);
@@ -14,6 +25,14 @@ function $l(selector) {
   return new DOMNodeCollection(nodeArr);
 }
 
-
-
 window.$l = $l;
+
+$l.extend = (first_obj,...args) => {
+  args.forEach((arg)=>{
+    let keys = Object.keys(arg);
+    keys.forEach((key)=>{
+      first_obj[key] = arg[key]
+    });
+  });
+  return first_obj;
+};
